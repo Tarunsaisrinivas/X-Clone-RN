20035fbb84d6c45e051340e4678e8b121166332e
Tarun Sai Srinivas <tarunsaisrinivas4@gmail.com>
2026-01-12 17:32:43 +0530

    added cors and middlewares

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,16 @@
 import express from "express";
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
-
+import cors from "cors";
+import { clerkMiddleware } from "@clerk/express";
 const app = express();
-connectDB();
+app.use(cors());
+app.use(express.json());
+app.use(clerkMiddleware());
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 app.listen(ENV.PORT, () => {
-  console.log("Server is running on port 3000");
+  connectDB();
+  console.log("Server is running on port ", ENV.PORT);
 });
