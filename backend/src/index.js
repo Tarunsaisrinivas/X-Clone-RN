9d3685b7255954cf8255f8dba236008ca783d7cf
Tarun Sai Srinivas <tarunsaisrinivas4@gmail.com>
2026-01-12 14:19:35 +0530

    fixed imports and updated package.json create a dev for running application

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,12 @@
 import express from "express";
+import { ENV } from "./config/env.js";
+import { connectDB } from "./config/db.js";
 
 const app = express();
-
-app.listen(3000, () => {
+connectDB();
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.listen(ENV.PORT, () => {
   console.log("Server is running on port 3000");
 });
