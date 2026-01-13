ceb7de4a0b0a7c7daa6a021bef731868c75c6e18
Tarun Sai Srinivas <tarunsaisrinivas4@gmail.com>
2026-01-13 22:31:58 +0530

    user routes and controllers are written

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
 import cors from "cors";
 import { clerkMiddleware } from "@clerk/express";
+import userRoutes from "./routes/user.route.js";
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -10,6 +11,7 @@ app.use(clerkMiddleware());
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+app.use("/api/users", userRoutes);
 app.listen(ENV.PORT, () => {
   connectDB();
   console.log("Server is running on port ", ENV.PORT);
---
+++ backend/src/routes/user.route.js
@@ -0,0 +1,18 @@
+import express from "express";
+import {
+  getUserProfile,
+  syncUser,
+  updateProfile,
+  getCurrentUser,
+  followUser,
+} from "../controllers/user.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
+
+const router = express.Router();
+
+router.get("/profile/:username", getUserProfile);
+router.post("/sync", protectRoute, syncUser);
+router.post("/me", protectRoute, getCurrentUser);
+router.put("/profile", protectRoute, updateProfile);
+router.post("/following",protectRoute,followUser);
+export default router;
